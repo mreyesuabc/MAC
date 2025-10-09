6f9fc508f1cffc1b46cfb15e36197b6809ffce2f
BAJA\mreyesr <mreyesr@baja.gob.mx>
2025-10-09 13:45:47 -0700

    CHANGE: Cambios en la presentacion de notificaciones y enla eliminacion de usuario

--- obj/Debug/net9.0/MAC.AssemblyInfo.cs
+++ obj/Debug/net9.0/MAC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f86ce9f620501b0f5e1c2f4af60373c85b07364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e955b67dff837ed5171fd81a5556c924bb54f7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
